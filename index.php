f53500d20adcdd10a6e8811e1edf1c78ea1f44ac
Oshiiro <benjamin.delaborde@laposte.net>
2016-11-23 14:21:01 +0100

    Changement background & style

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" title="no title">
     <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" title="no title">
+    <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" title="no title">
+    <link rel="stylesheet" href="css/style.css">
     <title>Chronique de Simon</title>
   </head>
   <body>
