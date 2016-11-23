e9a714ad3d0976a128b209931e95078ff9aaba92
Big-Bud <webforce321@gmail.com>
2016-11-23 15:37:30 +0100

    jfais dla merde

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 // Ceci représentera les aventures épique de Simon, et sa quête de l'oignon.
   // Introduction
   // Chapitre 1 : L'épopée de JL.
+  // N°1: Le saucisson tu aimera !!
+  // N°2 : La sodomie tu venèreras.
+  // Chaptire 3 : La sodomie tu venereras
+//zobe zobe
--- index.php
+++ index.php
@@ -2,10 +2,32 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" title="no title">
+    <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" title="no title">
+    <link rel="stylesheet" href="css/style.css">
     <title>Chronique de Simon</title>
   </head>
   <body>
     <h1>Chronique de Simon</h1>
-      <p>Ce site va vous raconter l'histoire incroyable des aventures Simonesque de Simon</p>
+    <p>Ce site va vous raconter l'histoire incroyable des aventures Simonesque de Simon</p>
+    <ul>
+      <li>salut</li>
+      <li>salut</li>
+      <li>salut</li>
+      <li>salut</li>
+      <li>salut</li>
+      <li>salut</li>
+    </ul>
+    <form class="" action="index.html" method="post">
+      <label for="01">test 01</label>
+      <input type="text" name="01" value="">
+      <button type="button" class="btn btn-default" name="button">wesh wesh wesh</button>
+      <p>enculé de ta mère</p>
+    </form>
+    <script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script>
+      $('.dropdown-toggle').dropdown()
+    </script>
   </body>
 </html>
