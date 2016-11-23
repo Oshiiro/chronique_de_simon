cdbc36a19a7c8c6301eb8dfa2bff2593c835b9db
Oshiiro <benjamin.delaborde@laposte.net>
2016-11-23 14:10:30 +0100

    Ajout de bootstrap et jquery

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" title="no title">
+    <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" title="no title">
     <title>Chronique de Simon</title>
   </head>
   <body>
@@ -19,9 +21,13 @@
     <form class="" action="index.html" method="post">
       <label for="01">test 01</label>
       <input type="text" name="01" value="">
-      <button type="button" class="btn btn-default" name="button"></button>
-      <p>enculé de ta mère</p>
       <button type="button" class="btn btn-default" name="button">wesh wesh wesh</button>
+      <p>enculé de ta mère</p>
     </form>
+    <script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script>
+      $('.dropdown-toggle').dropdown()
+    </script>
   </body>
 </html>
