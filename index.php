66d9c29feb47b928b25eaea3e9e63663fc9ce75e
Oshiiro <benjamin.delaborde@laposte.net>
2016-11-23 12:08:36 +0100

    base de l'index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Chronique de Simon</title>
+  </head>
+  <body>
+    <h1>Chronique de Simon</h1>
+      <p>Ce site va vous raconter l'histoire incroyable des aventures Simonesque de Simon</p>
+  </body>
+</html>
