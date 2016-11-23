34ed2b192e935e84617b02e3a889ff3c03c3e875
J-Hell-P <jerryleeperree@gmail.com>
2016-11-23 15:34:07 +0100

    modif index

--- index.php
+++ index.php
@@ -10,20 +10,16 @@
   <body>
     <h1>Chronique de Simon</h1>
     <p>Ce site va vous raconter l'histoire incroyable des aventures Simonesque de Simon</p>
+    <hr>
     <ul>
-      <li>salut</li>
-      <li>salut</li>
-      <li>salut</li>
-      <li>salut</li>
-      <li>salut</li>
-      <li>salut</li>
+      <li><a>Chapitre 1</a> : <h6>L'épopée JL</h6></li>
+      <li><a>Chapitre 2</a> : <h6>Jessy l'apotre</h6></li>
+      <li><a>Chapitre 3</a> : <h6>Kilian le Fétichiste arnarchiste</h6></li>
+      <li><a>Chapitre 4</a> : <h6>Benoit le Pape</h6></li>
+      <li><a>Chapitre 5</a> : <h6>Ben Le prophète de Laden</h6></li>
+      <li><a>Chapitre 6</a> : <h6>Mathieu le pieux</h6></li>
     </ul>
-    <form class="" action="index.html" method="post">
-      <label for="01">test 01</label>
-      <input type="text" name="01" value="">
-      <button type="button" class="btn btn-default" name="button">wesh wesh wesh</button>
-      <p>enculé de ta mère</p>
-    </form>
+
     <script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script>
