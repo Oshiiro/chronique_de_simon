8b4754470949bf816da22a5a2fa079d5d5ff5f6c
Oshiiro <benjamin.delaborde@laposte.net>
2016-11-23 16:45:35 +0100

    modif oshiiro

--- index.php
+++ index.php
@@ -3,31 +3,26 @@
 
     <h1>Chroniques de Simon</h1>
     <p>Ce site va vous raconter l'histoire incroyable des aventures Simonesque de Simon</p>
+    <a href="contact.php">Contact</a>
+    <a href="article.php">Articles</a>
     <hr>
-
-    <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 chapitres">
-      <ul>
-        <li><a>Chapitre 1</a> : <h6>L'épopée JL</h6></li>
-        <hr>
-        <li><a>Chapitre 2</a> : <h6>Jessy l'apotre</h6></li>
-        <hr>
-        <li><a>Chapitre 3</a> : <h6>Kilian le Fétichiste arnarchiste</h6></li>
-        <hr>
-        <li><a>Chapitre 4</a> : <h6>Benoit le Pape</h6></li>
-        <hr>
-        <li><a>Chapitre 5</a> : <h6>Ben Le prophète de Laden</h6></li>
-        <hr>
-        <li><a>Chapitre 6</a> : <h6>Mathieu le pieux</h6></li>
-      </ul>
+    <div class="container">
+      <div class="col-md-12 chapitres">
+        <ul>
+          <li><a>Chapitre 1</a> : <h6>L'épopée JL</h6></li>
+          <hr>
+          <li><a>Chapitre 2</a> : <h6>Jessy l'apotre</h6></li>
+          <hr>
+          <li><a>Chapitre 3</a> : <h6>Kilian le Fétichiste arnarchiste</h6></li>
+          <hr>
+          <li><a>Chapitre 4</a> : <h6>Benoit le Pape</h6></li>
+          <hr>
+          <li><a>Chapitre 5</a> : <h6>Ben Le prophète de Laden</h6></li>
+          <hr>
+          <li><a>Chapitre 6</a> : <h6>Mathieu le pieux</h6></li>
+        </ul>
+      </div>
     </div>
-<a href="contact.php">Contact</a>
-<a href="article.php">Articles</a>
-    <script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <script>
-      $('.dropdown-toggle').dropdown()
-    </script>
-  </body>
-</html>
+
 
 <?php include ('include/footer.php'); ?>
