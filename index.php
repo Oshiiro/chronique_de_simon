19f6c373d3cf3392369dd766bb626ea54a3e1883
J-Hell-P <jerryleeperree@gmail.com>
2016-11-23 15:41:57 +0100

    css accueil

--- index.php
+++ index.php
@@ -3,15 +3,21 @@
     <h1>Chronique de Simon</h1>
     <p>Ce site va vous raconter l'histoire incroyable des aventures Simonesque de Simon</p>
     <hr>
-    <ul>
-      <li><a>Chapitre 1</a> : <h6>L'épopée JL</h6></li>
-      <li><a>Chapitre 2</a> : <h6>Jessy l'apotre</h6></li>
-      <li><a>Chapitre 3</a> : <h6>Kilian le Fétichiste arnarchiste</h6></li>
-      <li><a>Chapitre 4</a> : <h6>Benoit le Pape</h6></li>
-      <li><a>Chapitre 5</a> : <h6>Ben Le prophète de Laden</h6></li>
-      <li><a>Chapitre 6</a> : <h6>Mathieu le pieux</h6></li>
-    </ul>
-
+    <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 chapitres">
+      <ul>
+        <li><a>Chapitre 1</a> : <h6>L'épopée JL</h6></li>
+        <hr>
+        <li><a>Chapitre 2</a> : <h6>Jessy l'apotre</h6></li>
+        <hr>
+        <li><a>Chapitre 3</a> : <h6>Kilian le Fétichiste arnarchiste</h6></li>
+        <hr>
+        <li><a>Chapitre 4</a> : <h6>Benoit le Pape</h6></li>
+        <hr>
+        <li><a>Chapitre 5</a> : <h6>Ben Le prophète de Laden</h6></li>
+        <hr>
+        <li><a>Chapitre 6</a> : <h6>Mathieu le pieux</h6></li>
+      </ul>
+    </div>
     <script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script>
