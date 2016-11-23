080e79c46d697fb480d10fe796e5b46206565fc4
Oshiiro <benjamin.delaborde@laposte.net>
2016-11-23 12:43:38 +0100

    modif index ajout menu

--- index.php
+++ index.php
@@ -8,5 +8,13 @@
   <body>
     <h1>Chronique de Simon</h1>
       <p>Ce site va vous raconter l'histoire incroyable des aventures Simonesque de Simon</p>
+      <ul>
+        <li>salut</li>
+        <li>salut</li>
+        <li>salut</li>
+        <li>salut</li>
+        <li>salut</li>
+        <li>salut</li>
+      </ul>
   </body>
 </html>
